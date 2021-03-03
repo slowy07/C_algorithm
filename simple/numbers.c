5c31f7074e62c414c3f9bcb41c3354ecc276ca23
slowy07 <slowy.arfy@gmail.com>
2021-03-03 20:51:03 +0700

    numbers

---
+++ simple/numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    
+    printf("float number : %f", 42.33);
+    printf("\n");
+    printf("decimal number: %d", 4 + 4);
+    printf("\n");
+    printf("number with pow() : %f \n", pow(3,2));
+    printf("number with ceil() : %f \n", ceil(46.72));
+    printf("number with floor() : %f", floor(36.25));
+
+    return 0;
+}
